1b0d8a8ea63a45840ac37495379667fdf03c49c7
Russ <156949879+emergencyrussell@users.noreply.github.com>
2024-07-07 18:26:54 -0700

    Code update

--- OpenOTP32/OpenOTP32.ino
+++ OpenOTP32/OpenOTP32.ino
@@ -5,8 +5,10 @@
 // Send secret messages to your friends :)
 
 #include <Adafruit_Thermal.h>
-// EspSoftwareSerial library:
 #include <SoftwareSerial.h>
+#include "esp_system.h"
+#include "driver/adc.h"
+#include "esp_adc_cal.h"
 
 // Define the RX and TX pins for the SoftwareSerial
 #define TX_PIN 17
@@ -23,21 +25,23 @@ Adafruit_Thermal printer(&mySerial);
 void setup() {
   // Start the software serial communication
   mySerial.begin(9600);
-
+  
+  //bootloader_random_enable();
+  esp_adc_cal_characteristics_t *adc_chars = (esp_adc_cal_characteristics_t*) calloc(1, sizeof(esp_adc_cal_characteristics_t));
+  adc1_config_width(ADC_WIDTH_BIT_12);
+  adc1_config_channel_atten(ADC1_CHANNEL_0, ADC_ATTEN_DB_0);
+  esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_0, ADC_WIDTH_BIT_12, 1100, adc_chars);
+  
   // Create empty String to populate with random numbers
   String randSeq = "";
-
+  
   // Preparing the String and formatting into groups of five
   for (int i = 0; i < sequenceLength; i++) {
     uint32_t randomDigit = esp_random() % 10; // Gen random digit
     randSeq += String(randomDigit);
-    
-	// How many digits per group (DPG; default, five)
-	if ((i + 1) % 5 == 0) {
+    if ((i + 1) % 5 == 0) {
       randSeq += " ";
     }
-	
-	// How many digits per line (DPL; default, twenty-five)
     if ((i + 1) % 25 == 0) {
       randSeq += "\n";
     }
--- README.md
+++ README.md
@@ -13,6 +13,10 @@ I hope to elevate this project higher than that of a novelty toy, but people muc
 
 *1. Is the ESP32 RNG sufficiently unpredictable?*
 
+The ESP32 RNG is hardware based and ["samples physical noise are continuously mixed into the internal hardware RNG state to provide entropy."](https://www.espressif.com/sites/default/files/documentation/esp32_datasheet_en.pdf)
+
+	A data sample of 2 GB, which is read from the random number generator at a rate of 5 MHz with only the highspeed ADC being enabled, has been tested using the Dieharder Random Number Testsuite (version 3.31.1). The sample passed all tests.
+
 *2. Do the WiFi/Bluetooth connectivity features of the ESP32 present a vulnerability?*
 
 # Contents
